c93d22016701a6e50c256a8170700a4170f77f15
Alexis Decker <deckera@hawaii.edu>
2023-09-13 15:16:45 -1000

    I've completed Part 8:" On My Own and attached my screenshot on GitHub

--- HackwichTwo_9.5.23/ViewController.swift
+++ HackwichTwo_9.5.23/ViewController.swift
@@ -11,11 +11,26 @@ import UIKit
 class ViewController: UIViewController {
     //Part 8: this is where i want you to declare 3 string variables
 
+    var stringOne = "the fox"
+    var stringTwo = " fell asleep."
+    var stringThree = " "
+    
     override func viewDidLoad() {
+        
+    stringThree = stringOne + stringTwo
+        
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func partEightButtonPressed(_ sender: Any)
+    {
+        print(stringThree)
+    }
+    
+    
     @IBAction func pressMeButtonPressed(_ sender: Any)
     {
         //code block write here
